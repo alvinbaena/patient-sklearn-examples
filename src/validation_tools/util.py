01b9dec0dd104dec39318cec37561f40f9bf40b7
Andres Moreno <andres.moreno.barbosa@gmail.com>
2016-06-10 16:35:53 -0500

    fixed C exploration

--- src/validation_tools/util.py
+++ src/validation_tools/util.py
@@ -9,8 +9,21 @@ def find_new_explore_c(exploreC, best_val):
     index_val = exploreC.index(best_val)
     distance_next = float('inf') if index_val == len(exploreC) - 1 else float(exploreC[index_val + 1] - exploreC[index_val])
     distance_prev = float('inf') if index_val == 0 else float(exploreC[index_val] - exploreC[index_val - 1])
-    new_range = min(distance_next, distance_prev)
-    min_val = 0.0 if best_val - new_range < 0 else float(best_val - new_range)
-    max_val = float(best_val + new_range)
-    exploreC = np.linspace(min_val, max_val, len(exploreC)).tolist()
+    if index_val!=0 and index_val!=len(exploreC):
+        exploreC = np.linspace(exploreC[index_val - 1]+distance_prev/2, exploreC[index_val + 1]-distance_next/2, len(exploreC)).tolist()
+            
+        
+   
+    else:
+        new_range=min(distance_next,distance_prev)
+        min_val = 0.0001 if best_val - new_range <= 0 else float(best_val - new_range)
+        max_val = float(best_val + new_range)
+        if index_val==0:
+            exploreC = np.linspace(min_val, exploreC[index_val + 1]-distance_next/2, len(exploreC)).tolist()
+            
+        else:
+            exploreC = np.linspace(exploreC[index_val - 1]+distance_prev/2, max_val, len(exploreC)).tolist()
+        
+    
+    print exploreC
     return exploreC
